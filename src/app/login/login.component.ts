e44249163ce1d227c1ba39176efbec339b2b82f3
Shivank Shekhar <31924615+ShivankShekhar@users.noreply.github.com>
2019-09-30 00:51:32 +0530

    Add files via upload

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,77 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
-import * as angula from "angular";
-
+import { Router } from '@angular/router';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
-  public userId:string="";
-  public pass:string="";  
-  router: Router; 
-  constructor(_router: Router) {  
-    this.userId="";
-    this.pass="";
+  public userId: string = "";
+  public pass: string = "";
+  router: Router;
+
+  constructor(_router: Router, private http: Http) {
+    this.userId = "1501021023";
+    this.pass = "123456";
     this.router = _router;
   }
 
   ngOnInit() {
   }
- 
+
+  login() {
+
+    console.log("entered");
+    this.http.get(`http://localhost:8000/stu-login?ID=${this.userId}&Password=${this.pass}`).subscribe((res) => {
+      var dat = JSON.stringify(res);
+      var data = JSON.parse(dat)._body;
+      if (data === 'false') {
+        console.log("wrong");
+        alert("Invalid Credentials \n Please Try again!");
+      }
+      else if (data === 'true') {
+        console.log("right");
+        console.log("User Logged In");
+        this.router.navigateByUrl("/Student/Logged");
+      }
+    });
+
+  }
 
 
-login(){  
-  var data={
-    ID:this.userId,
-    Password:this.pass
-  }; 
-  var http = new XMLHttpRequest();
-  http.open("POST","http://localhost:8000/stu-login",true);
-  http.setRequestHeader("Content-Type","application/json");
-  http.send(JSON.stringify(data));
-        http.onreadystatechange = function(){
-          if(http.readyState===4)
-          {
-            if(this.responseText==='false')
-            { 
-              alert("Invalid Credentials \n Please Try again!");  
-            }
-            else
-            { 
-              console.log("User Logged In"); document.getElementById("res").innerHTML = this.responseText;
-            }
-          }
-            };
-    console.log(http.status); 
-    
-  }  
-
-// click(){
-  
-//   var app=angular.module('myApp',[]);
-//   app.controller('myCtrl',function($scope,$http)
-//   {
-//     $scope.submit=function(){
-//         var data=$.param({
-//         dat: JSON.stringify({
-//             ID:$scope.userId,
-//             Password:$scope.pass
-//           }) 
-//       });
-    
-//       $http.POST("http://localhost:8000/stu-login",data).success(function(data,status){ 
-//         console.log("data posted");
-//       });
-//     }
-//   });
-//   }
-
-   mov(){
-    this.login(); 
-    //this.click();
-    //  if( x) this.router.navigateByUrl('Student/Logged');
-    }
 }
